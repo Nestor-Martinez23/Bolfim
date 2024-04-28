8d8760141d8e26818d37f37b72b9ac9f167ab885
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-04-27 21:32:19 -0600

    Update proyecto

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <div className="contenedor">
+      <main>
+      <div className="left-section">
+        <h1>BOLFIM</h1>
+        {/* Aquí puedes agregar el código para la imagen de la empresa */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="right-section" >
+        {/* Aquí puedes agregar el código para el formulario de inicio de sesión */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </main>
+      <footer className="footer">
+        <p>BOLFIM S.A de C.V.</p>
+      </footer>
+    </div>
   )
 }
 
