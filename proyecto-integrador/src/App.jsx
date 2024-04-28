44b5604e81d4651f8f18f03e2d40519d657886c9
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-04-28 16:40:03 -0600

    Update app.jsx

--- proyecto-integrador/src/App.jsx
+++ proyecto-integrador/src/App.jsx
@@ -6,11 +6,27 @@ function App() {
     <div className="contenedor">
       <main>
       <div className="left-section">
-        <h1>BOLFIM</h1>
+        <header>
+          <h1>BOLFIM</h1>
+        </header>
+        <img src="src\bolfim.png" alt="" />
         {/* Aquí puedes agregar el código para la imagen de la empresa */}
       </div>
       <div className="right-section" >
-        {/* Aquí puedes agregar el código para el formulario de inicio de sesión */}
+      <h2>Login</h2>
+          <form>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+            </div>
+
+            <div>
+                <label htmlFor="password">Contraseña:</label>
+                <input type="password" id="password" name="password" required />
+            </div>
+            <button type="submit">Iniciar sesión</button>
+          </form>
+          <p>Para soporte técnico, por favor contacta a: soporte@bolfim.com</p>
       </div>
       </main>
       <footer className="footer">
