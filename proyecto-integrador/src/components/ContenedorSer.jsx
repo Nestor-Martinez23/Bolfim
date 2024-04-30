b501f5fde549fe1ae0acec5ff607af45de514b18
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-04-29 19:13:00 -0600

    Update ContenedorSer

---
+++ proyecto-integrador/src/components/ContenedorSer.jsx
@@ -0,0 +1,12 @@
+import '../styles/ContenedorSer.css';
+import Ordenes from '../services/Ordenes.jsx';
+
+function ContenedorSer() {
+    
+      
+    return (
+        Ordenes()
+    )
+}
+
+export default ContenedorSer;
