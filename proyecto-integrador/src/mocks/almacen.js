3fac795881b5645ccf58be5c03327e27734777ab
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-04 17:49:54 -0600

    feat: Add almacen mock data

---
+++ proyecto-integrador/src/mocks/almacen.js
@@ -0,0 +1,28 @@
+const materiaPrima = [
+  {
+    id: '1',
+    name: 'Materia Prima 1',
+    date: '2022-01-01',
+    weight: 10,
+  },
+  {
+    id: '2',
+    name: 'Materia Prima 2',
+    date: '2022-01-02',
+   weight: 15,
+  },
+  {
+    id: '3',
+    name: 'Materia Prima 3',
+    date: '2022-01-03',
+    weight: 20,
+  },
+  {
+    id: '4',
+    name: 'Materia Prima 4',
+    date: '2022-01-04',
+    weight: 25,
+  },
+];
+
+export default materiaPrima;
