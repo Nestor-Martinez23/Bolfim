23e8ac5c2447c7831ccc0c7c14822f11e6c39a8f
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-04 17:49:32 -0600

    feat: Update Login component with user authentication functionality

--- proyecto-integrador/src/components/Login.jsx
+++ proyecto-integrador/src/components/Login.jsx
@@ -1,29 +1,29 @@
 import '../styles/Login.css';
 import bolfimLogo from '../assets/bolfim.png';
-//
-function Login() {
+import useUserAuth from '../hooks/useUserAuth.jsx';
+
+function Login({ onLoginSuccess }) {
+    const { emailRef, passwordRef, handleSubmit } = useUserAuth({ onLoginSuccess });
+
     return (
         <div className="contenedor-login">
-            
             <main>
                 <div className="left-section-login">
                     <header>
                         <h2>BOLFIM</h2>
                     </header>
                     <img src={bolfimLogo} alt="BOLFIM Logo" />
-                    
                 </div>
                 <div className="right-section-login">
                     <h1 className='h1-login'>Login</h1>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div>
                             <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email" ref={emailRef} required />
                         </div>
-
                         <div>
                             <label htmlFor="password">Contraseña:</label>
-                            <input type="password" id="password" name="password" required />
+                            <input type="password" id="password" name="password" ref={passwordRef} required />
                         </div>
                         <button className='button-login' type="submit">Iniciar sesión</button>
                     </form>
@@ -34,7 +34,7 @@ function Login() {
                 <p>BOLFIM S.A de C.V.</p>
             </footer>
         </div>
-    )
+    );
 }
-
+ 
 export default Login;
