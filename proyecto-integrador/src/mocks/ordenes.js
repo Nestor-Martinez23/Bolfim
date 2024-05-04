56c6422e253746ce89b607f5534dbbd0f2b5d77b
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-04 17:49:58 -0600

    feat: Agregar datos de órdenes desde archivo de simulación a componente Ordenes

---
+++ proyecto-integrador/src/mocks/ordenes.js
@@ -0,0 +1,26 @@
+const orders = [
+  {
+    id: '1',
+    info: 'Información de la orden 1',
+    progress: 25,
+  },
+  {
+    id: '2',
+    info: 'Información de la orden 2',
+    progress: 50,
+  },
+  {
+    id: '3',
+    info: 'Información de la orden 3',
+    progress: 75,
+  },
+  {
+    id: '4',
+    info: 'Información de la orden 4',
+    progress: 100,
+  },
+  // Agrega más órdenes aquí si es necesario
+];
+
+export default orders;
+  
