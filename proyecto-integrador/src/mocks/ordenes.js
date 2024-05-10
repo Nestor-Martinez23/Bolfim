2aafa11ef4bcb1ce4dbc0fed50edb6aa633e44a0
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-10 15:22:42 -0600

    feat: Add new orders to mock data

--- proyecto-integrador/src/mocks/ordenes.js
+++ proyecto-integrador/src/mocks/ordenes.js
@@ -19,6 +19,36 @@ const orders = [
     info: 'Información de la orden 4',
     progress: 100,
   },
+    {
+    id: '5',
+    info: 'Información de la orden 1',
+    progress: 25,
+  },
+  {
+    id: '6',
+    info: 'Información de la orden 1',
+    progress: 25,
+  },
+  {
+    id: '7',
+    info: 'Información de la orden 2',
+    progress: 50,
+  },
+  {
+    id: '8',
+    info: 'Información de la orden 3',
+    progress: 75,
+  },
+  {
+    id: '9',
+    info: 'Información de la orden 4',
+    progress: 100,
+  },
+    {
+    id: '10',
+    info: 'Información de la orden 1',
+    progress: 25,
+  },
   // Agrega más órdenes aquí si es necesario
 ];
 
