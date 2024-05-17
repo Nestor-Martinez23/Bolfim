cab8ff0b888d313db54d3768d4976abc5cd219f8
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-16 23:45:10 -0600

    feat: Add paneles compont to control users and rols

---
+++ proyecto-integrador/src/components/Paneles.jsx
@@ -0,0 +1,53 @@
+import padlock from '../assets/panel-padlock.svg';
+import paneluser from '../assets/panel-user.svg';
+
+function PanelMenu({setPanel}) {
+    return (
+        <>
+            <section className='contenedor-paneles'>
+                <h2>Menu control</h2>
+                <div className='panel-rol'>
+                    <h3>Permisos/Roles</h3>
+                    <a href="#" onClick={() => setPanel(2)}>
+                        <img src={padlock} alt="padlock" />
+                    </a>
+                </div>
+                <div className="panel-user">
+                    <h3>Usuarios</h3>
+                    <a href="#" onClick={() => setPanel(1)}>
+                        <img src={paneluser} alt="imagen de usuario" />
+                    </a>
+                </div>
+            </section>
+            <div className='scroll-bar-panel'></div>
+        </>
+    );
+}
+
+function PanelUsers({setPanel}) {
+    return (
+        <>
+            <section className='contenedor-paneles'>
+                <h2>Menu Usuarios</h2>
+                <div className="panel-user"></div>
+                <a className="boton_back" href="#" onClick={() => setPanel(0)}>Volver</a>
+            </section>
+            <div className='scroll-bar-panel'></div>
+        </>
+    );
+}
+
+function PanelRol({setPanel}) {
+    return (
+        <>
+            <section className='contenedor-paneles'>
+                <h2>Menu Roles</h2>
+                <div className='panel-rol'></div>
+                <a className="boton_back" href="#" onClick={() => setPanel(0)}>Volver</a>
+            </section>
+            <div className='scroll-bar-panel'></div>
+        </>
+    );
+}
+
+export { PanelMenu, PanelUsers, PanelRol };
