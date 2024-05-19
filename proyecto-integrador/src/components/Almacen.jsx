72ebebd30805ec13dd21de44ca90d08bf2221edb
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-18 18:03:04 -0600

    feat: Update Almacen component to fetch and display data from API

--- proyecto-integrador/src/components/Almacen.jsx
+++ proyecto-integrador/src/components/Almacen.jsx
@@ -1,17 +1,34 @@
-import materiaPrima from "../mocks/almacen.js";
+
 import IconDats from "../assets/icons/dots-vertical.svg";
+import IconCase from "../assets/icons/briefcase.svg";
+import { useState, useEffect } from "react";
+import { getAlmacen } from "../services/crudAlmacen.js";
 function Almacen() {
+  const [materiaPrima, setMateria] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+        try {
+            const products = await getAlmacen();
+            setMateria(products);
+        }
+        catch (error) {
+            console.error('Error al obtener los productos:', error);
+        }
+    }
+    fetchData();
+}, []);
    
     
     return (
         <>
             <section className='contenedor-products'>
                 {materiaPrima.map((materia) => (
-                    <div className='product' key={materia.id}>
-                        <div>{materia.id}</div>
+                    <div className='product' key={materia._id}>
+                        <div> <img src={IconCase} alt="icono de case" /></div>
                         <div>{materia.name}</div>
                         <div>{materia.date}</div>
-                        <div >{materia.weight}</div>
+                        <div >{materia.weight} Kg</div>
                         <div>      <img src={IconDats} alt="eliminar" /></div>
                     </div>
                 ))}
@@ -26,6 +43,17 @@ function Almacen() {
 function NavAlmacen(){
   return (
     <nav>
+      <div></div>
+      <div></div>
+      <div></div>
+      <a  href=""> <span></span>Volver</a> 
+    </nav>
+  )
+}
+
+/*function NavAlmacen(){
+  return (
+    <nav>
       <a  href=""> <span></span>Volver</a>
       <a href="">Materia Prima</a>
       <a href="">Materia Prima en</a>
@@ -34,5 +62,6 @@ function NavAlmacen(){
     </nav>
   )
 }
+*/
 
 export {Almacen, NavAlmacen};
