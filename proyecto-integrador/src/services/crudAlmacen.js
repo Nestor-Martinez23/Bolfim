e36c1a8e66132da67171f8ea6bd63ab87fd0c241
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-23 23:42:34 -0600

    feat: Agregar funciones para crear, actualizar y eliminar materias primas

--- proyecto-integrador/src/services/crudAlmacen.js
+++ proyecto-integrador/src/services/crudAlmacen.js
@@ -1,4 +1,4 @@
-const URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/';
 
 async function getAlmacen() {
     try {
@@ -19,4 +19,64 @@ async function getAlmacen() {
     
 }
 
-export { getAlmacen };
+async function createAlmacen(newMateria) {
+    console.log(newMateria);
+    try {
+        const response = await fetch(`${URL}CreateAlmacen`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newMateria)
+        }) 
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error al crear un nuevo materia:', error);
+        throw error;
+        
+    }
+}
+
+// Función para actualizar un materia
+async function updateAlmacen(id_materia, materiaUpdated) {
+    console.log("a actualizar:",id_materia, materiaUpdated);
+    try {
+        const response = await fetch(`${URL}UpdateAlmacen/${id_materia}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(materiaUpdated)
+        });
+        const data = await response.json();
+        // Manejar la respuesta
+        return data;
+    } catch (error) {
+        // Manejar errores de la solicitud
+        console.error('Error al actualizar materia:', error);
+        throw error;
+    }
+}
+
+// Función para eliminar materia
+async function deleteAlmacen(id_materia) {
+    console.log(id_materia);
+    try {
+        const response = await fetch(`${URL}DeleteAlmacen/${id_materia}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        // Manejar la respuesta
+        return data;
+    } catch (error) {
+        // Manejar errores de la solicitud
+        console.error('Error al eliminar la materia:',error.status);
+        throw error;
+    }
+}
+
+export { getAlmacen, createAlmacen, updateAlmacen, deleteAlmacen};
