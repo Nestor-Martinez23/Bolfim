962871f5c557a10b616d36432c0b147b1e3af3ed
Nestor-Martinez23 <emiliano.martinezmrn@uanl.edu.mx>
2024-05-23 23:42:30 -0600

    feat: Update CheckUserLog.js to use API URL from environment variable

--- proyecto-integrador/src/services/CheckUserLog.js
+++ proyecto-integrador/src/services/CheckUserLog.js
@@ -1,4 +1,4 @@
-const URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/';
 const checkUser = async (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
